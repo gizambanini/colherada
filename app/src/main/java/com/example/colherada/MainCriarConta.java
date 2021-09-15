d8e83f2f2c4184403f2187de45e71a93f23776e8
Milena Furuta Shishito <milenafurutashishito@gmail.com>
2021-09-15 16:41:48 -0300

    project created

---
+++ app/src/main/java/com/example/colherada/MainCriarConta.java
@@ -0,0 +1,14 @@
+package com.example.colherada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainCriarConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_criar_conta);
+    }
+}
---
+++ app/src/main/java/com/example/colherada/MainMeusDados.java
@@ -0,0 +1,14 @@
+package com.example.colherada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMeusDados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_meus_dados);
+    }
+}
