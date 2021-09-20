f980aae94ac06e3b7ac20f090dac372eb2aa5ff9
Milena Furuta Shishito <milenafurutashishito@gmail.com>
2021-09-19 22:59:15 -0300

    pagina home

--- app/src/main/java/com/example/colherada/MainCalorias.java
+++ app/src/main/java/com/example/colherada/MainCalorias.java
@@ -3,6 +3,13 @@ package com.example.colherada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainCalorias extends AppCompatActivity {
 
