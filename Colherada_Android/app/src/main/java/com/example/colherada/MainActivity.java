e8e05d38733b1f896bd5c07ab6b414fc3a41e543
Milena Furuta Shishito <milenafurutashishito@gmail.com>
2021-10-03 23:02:40 -0300

    nova tela de Receitas e atualização da Home

--- Colherada_Android/app/src/main/java/com/example/colherada/MainActivity.java
+++ Colherada_Android/app/src/main/java/com/example/colherada/MainActivity.java
@@ -1,82 +1,72 @@
 package com.example.colherada;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btnCalorias, btnTesteIsa;
-    //LinearLayout layout;
+    Button btnCalorias, btnReceitas;
+    CardView cardView1, cardView2, cardView3;
+    TextView tvImg1, tvImg2, tvImg3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
         btnCalorias = (Button) findViewById(R.id.btnCalorias);
-        btnTesteIsa = (Button) findViewById(R.id.btnReceitas);
-//        layout = (LinearLayout) findViewById(R.id.layout);
+        btnReceitas = (Button) findViewById(R.id.btnReceitas);
+        cardView1 = (CardView) findViewById(R.id.CardView1);
+        cardView2 = (CardView) findViewById(R.id.CardView2);
+        cardView3 = (CardView) findViewById(R.id.CardView3);
+        tvImg1 = (TextView) findViewById(R.id.tvImg1);
+        tvImg2 = (TextView) findViewById(R.id.tvImg2);
+        tvImg3 = (TextView) findViewById(R.id.tvImg3);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        cardView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (btn1.getText() == "")
+                if (tvImg1.getText() == "Sobre a iniciativa")
                 {
-                    btn2.setLayoutParams(new LinearLayout.LayoutParams(810, 450));
-                    btn3.setLayoutParams(new LinearLayout.LayoutParams(810, 400));
-                    btn2.setText("");
-                    btn3.setText("");
-
-                    btn1.setLayoutParams(new LinearLayout.LayoutParams(810, 780));
-                    btn1.setText("Bem-vindo(a) á Colherada! \n \"Uma colherada de sabor!\"" +
+                    tvImg2.setText("Bem-vinda(o) ao mundo da comida!");
+                    tvImg3.setText("Conheça todas os tipos de receita desponíveis");
+                    tvImg1.setText("Sobre a iniciativa\n\nBem-vindo(a) á Colherada!  \"Uma colherada de sabor!\"" +
                             "Projeto criados pela Giovana Zamabanini, " +
-                            "Isabela Clementino e Milena Shishito");
+                            "Isabela Clementino e Milena Shishito do Colégio Técnico de Campinas - COTUCA");
                 } else {
-                    btn1.setText("");
-                    btn1.setLayoutParams(new LinearLayout.LayoutParams(810, 430));
+                    tvImg1.setText("Sobre a iniciativa");
                 }
 
             }
         });
-        btn2.setOnClickListener(new View.OnClickListener() {
+        cardView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (btn2.getText() == "")
+                if (tvImg2.getText() == "Bem-vinda(o) ao mundo da comida!")
                 {
-                    btn1.setLayoutParams(new LinearLayout.LayoutParams(810, 430));
-                    btn3.setLayoutParams(new LinearLayout.LayoutParams(810, 400));
-                    btn1.setText("");
-                    btn3.setText("");
-
-                    btn2.setLayoutParams(new LinearLayout.LayoutParams(810, 680));
-                    btn2.setText("Encontre receitas práticas pro seu dia-a-dia!!");
+                    tvImg1.setText("Sobre a iniciativa");
+                    tvImg3.setText("Conheça todas os tipos de receita desponíveis");
+                    tvImg2.setText("Bem-vinda(o) ao mundo da comida!\n\nEncontre receitas práticas pro seu dia-a-dia!!\n " +
+                            "Na Colherada qualquer um pode compartilhara suas melhores receitas dígnas de uma boa fama!!! " +
+                            "Além de ajudar muitos que estavam precisando daquela ajudinha na cozinha.");
                 } else {
-                    btn2.setText("");
-                    btn2.setLayoutParams(new LinearLayout.LayoutParams(810, 450));
+                    tvImg2.setText("Bem-vinda(o) ao mundo da comida!");
                 }
             }
         });
-        btn3.setOnClickListener(new View.OnClickListener() {
+        cardView3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (btn3.getText() == "")
+                if (tvImg3.getText() == "Conheça todas os tipos de receita desponíveis")
                 {
-                    btn1.setLayoutParams(new LinearLayout.LayoutParams(810, 430));
-                    btn2.setLayoutParams(new LinearLayout.LayoutParams(810, 450));
-                    btn1.setText("");
-                    btn2.setText("");
-
-                    btn3.setLayoutParams(new LinearLayout.LayoutParams(810, 880));
-                    btn3.setText("- Carne/Frango/Peixe" +
+                    tvImg1.setText("Sobre a iniciativa");
+                    tvImg2.setText("Bem-vinda(o) ao mundo da comida!");
+                    tvImg3.setText("Conheça todas os tipos de receita desponíveis\n\n- Carne/Frango/Peixe" +
                             "- Massas" +
                             "- Lanches" +
                             "- Sobremesas" +
@@ -87,29 +77,24 @@ public class MainActivity extends AppCompatActivity {
                             "- Sem glúten/Sem lactose/Sem açúcar" +
                             "- Outros (Sucos/vitaminas/...)");
                 } else {
-                    btn3.setText("");
-                    btn3.setLayoutParams(new LinearLayout.LayoutParams(810, 450));
+                    tvImg3.setText("Conheça todas os tipos de receita desponíveis");
                 }
             }
         });
         btnCalorias.setOnClickListener ( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(MainActivity.this,MainCalorias.class);
-
                 startActivity(intent);
-
+                finish();
             }
         });
-        btnTesteIsa.setOnClickListener ( new View.OnClickListener() {
+        btnReceitas.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this,MainMinhasReceitas.class);
-
+                Intent intent = new Intent(MainActivity.this,MainReceitas.class);
                 startActivity(intent);
-
+                finish();
             }
         });
     }
